fbde2728bbd318ff8361ee7789b9b46ddb4a6225
Wibuakut1 <122853804+Wibuakut1@users.noreply.github.com>
2025-05-11 07:28:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', async () => {
   const { createClient } = supabase
 
-  const supabaseUrl = 'https://qjtcemqqrdrykuxutuxd.supabase.co'
-  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqdGNlbXFxcmRyeWt1eHV0dXhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4MDE3NTksImV4cCI6MjA2MjM3Nzc1OX0.1k4vyH-GsvBYvvPARf6f0eKtOj3ZFjmjJIV1GsdorvE'
+  const supabaseUrl = 'https://hvkddxotqatfuegekvjf.supabase.co'
+  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2a2RkeG90cWF0ZnVlZ2VrdmpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4MzYxNzcsImV4cCI6MjA2MjQxMjE3N30.u8cGDKrB_a5NYCHyHvpzaO08vAkj9Q1vkfCkvY_U6aU'
   const supabaseClient = createClient(supabaseUrl, supabaseKey)
 
   const form = document.getElementById('gameForm')
